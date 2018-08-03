13c306f6635034addb00813a135fa309e12621b7
Piotr Mankowski <pmanko@uw.edu>
2018-08-03 12:25:59 -0700

    Added CLI functionality

--- spec/labtime_spec.rb
+++ spec/labtime_spec.rb
@@ -1,5 +1,14 @@
-require 'spec_helper'
-require 'labtime'
+RSpec.describe Labtime do
+  it "has a version number" do
+    expect(Labtime::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
+
+
 
 describe Labtime::Labtime do
   def times_equal?(labtime, time_hash)
